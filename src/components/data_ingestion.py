0945eb3217a8fa52b36d80c7028a793300e70b7d
Dakshhhhh-ops <24bee040@nith.ac.in>
2026-06-15 00:14:31 +0530

    Added data ingestion component

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,59 @@
+import os
+import sys
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+
+from dataclasses import dataclass
+from src.logger import logging
+from src.exception import CustomException
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path=os.path.join("artifacts","train.csv")
+    test_data_path=os.path.join("artifacts","test.csv")
+    raw_data_path=os.path.join("artifacts","raw.csv")
+
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+
+
+    def initiate_data_ingestion(self):
+        logging.info("Data Ingestion process initiated")
+
+        try:
+            df = pd.read_csv("notebook/spam.csv", encoding="latin-1")
+            logging.info("original dataset reading done")
+
+                # Create Artifacts Folder
+            os.makedirs("artifacts", exist_ok=True)
+
+                #saving raw data
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header =True)
+            logging.info("Raw data saved")
+
+                #train test split
+            train_set,test_set=train_test_split(df, test_size=0.2, random_state=42)
+
+                #now to save train and test data
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            logging.info("Train and test paths saved")
+
+            return(self.ingestion_config.train_data_path,
+                       self.ingestion_config.test_data_path)
+            
+        except Exception as e:
+            raise CustomException(e,sys)
+            
+
+if __name__ == "__main__":
+    try:
+        obj = DataIngestion()
+        train_data, test_data = obj.initiate_data_ingestion()
+        print(f"Kaam khatam! Train path: {train_data}")
+
+    except Exception as e:
+        print(e)
